02e38e1af0783edcf4a948dc225cbb1f01539303
patelShas <patel.shas@northeastern.edu>
2022-12-03 14:33:10 -0500

    CRUD operations with react reduc

--- src/reviews/reviewItem.js
+++ src/reviews/reviewItem.js
@@ -1,3 +1,6 @@
+import {deleteReview, incrementLikes} from "../reducers/reviews-reducer";
+import {useDispatch} from "react-redux";
+
 const ReviewItem = (
     {
         review = {
@@ -5,10 +8,18 @@ const ReviewItem = (
             "text": "I found this by scrolling through their discography",
             "score": "3",
             "date": "2022-12",
-            "reviewer": "Alice"
+            "reviewer": "Alice",
+            "likes": 12
         }
 
     },) => {
+    const dispatch = useDispatch()
+    const deleteHandler = (id) => {
+        dispatch(deleteReview(id))
+    }
+    const likeHandler = (id) => {
+        dispatch(incrementLikes(id))
+    }
     return (
         <div>
             <div className={"d-flex flex-row justify-content-between"}>
@@ -19,11 +30,20 @@ const ReviewItem = (
                     <span className={"fw-bold"}> • {review.date}</span>
                 </div>
                 <div>
-                    Weekend at bernies
+                    <button type="button" className="btn btn-danger" onClick={() => {deleteHandler(review._id)}}>Farm upstate</button>
                 </div>
             </div>
             <br/>
-            {review.text}
+            <div className={"d-flex flex-row justify-content-between"}>
+                <div>
+                    {review.text}
+                </div>
+                <div onClick={() => {
+                    likeHandler(review._id)
+                }}>
+                    {review.likes}👍
+                </div>
+            </div>
         </div>
     )
 }
--- src/reviews/reviewWriter.js
+++ src/reviews/reviewWriter.js
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import {addReview} from "../reducers/reviews-reducer";
+import {useDispatch} from "react-redux";
 
 const ReviewWriter = (
     {
@@ -11,6 +13,7 @@ const ReviewWriter = (
     const [rev, setRev] = useState({
         text: '',
     });
+    const dispatch = useDispatch()
     const newRevHandler = (event) => {
         const revValue = event.target.value;
         const newRev = {
@@ -20,13 +23,14 @@ const ReviewWriter = (
     }
     const submitHandler = () => {
         const newReview = {
+            album_id: album._id,
             text: rev.text,
             score: "0",
             date: `${date.getFullYear()} - ${date.getMonth()}`,
             reviewer: "Bob",
             likes: 0
         };
-        console.log(newReview.text)
+        dispatch(addReview(newReview))
     }
     return (
         <div className={"list-group-item d-flex flex-row justify-content-between"}>
