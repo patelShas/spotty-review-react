310ece0d1b1c5ddcdc324f85d1d86c53811cf4de
nick-coury <nick.coury@sbcglobal.net>
2022-12-13 16:06:31 -0500

    fixed an import

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomeComponent from "./home/homeComponent";
 import SearchComponent from "./search/searchComponent";
 import DetailsComponent from "./details/detailsComponent";
 import LoginComponent from "./login/loginComponent";
+import RegisterComponent from "./register/registerComponent";
 
 function App() {
     return (<div className="container">
