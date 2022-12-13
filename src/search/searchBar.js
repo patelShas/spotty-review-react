1d26d504b38fa22c8df3b13d17c84888380de9a9
Ari <59126203+aleichtm@users.noreply.github.com>
2022-12-13 14:03:35 -0500

    switch from useLocation to Link in searchComponent

--- src/search/searchBar.js
+++ src/search/searchBar.js
@@ -1,21 +1,17 @@
 import {useState} from "react";
-import {useLocation} from "react-router";
+import {Link} from "react-router-dom";
 
 const SearchBar = () => {
-    const {pathname} = useLocation()
-    const root = pathname.split("/")[0]
     const [query, setQuery] = useState("");
     const SearchTypeHandler = (event) => {
         setQuery(event.target.value)
     }
-    return (
-        <div className={"list-group-item d-flex flex-row justify-content-between"}>
-            <input className="form-control" value={query} onChange={SearchTypeHandler}/>
-            <a href={`${root}/search/${query}`}>
-                <input type="submit" value="Search" className="btn btn-primary"/>
-            </a>
-        </div>
-    )
+    return (<div className={"list-group-item d-flex flex-row justify-content-between"}>
+        <input className="form-control" value={query} onChange={SearchTypeHandler}/>
+        <Link to={`${query}`}>
+            <input type="submit" value="Search" className="btn btn-primary"/>
+        </Link>
+    </div>)
 }
 
 export default SearchBar;
